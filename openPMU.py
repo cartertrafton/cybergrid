66f22acb8789a5713b5d4a08607f572aa9c70c13
mishakh <kharitonovm@wit.edu>
2020-06-15 13:12:56 -0400

    Added support for windows CMD terminal creation

--- openPMU.py
+++ openPMU.py
@@ -1,9 +1,16 @@
 import subprocess
 import os
+import sys
+
+if sys.platform == "linux":
+    cmdP = 'gnome-terminal -x'
+elif sys.platform == 'win32':
+    cmdP = 'cmd'
+
 pmuCall = 'PMU 1 127.0.0.1 1410 2048 true'
 pdcCall = 'PDC 1 127.0.0.1 1410 2048 true'
 
-print(pmuCall)
-print(pdcCall)
-
-os.system('gnome-terminal -x python3 pmuNet.py PMU 1 127.0.0.1 1410 2048 true')
+os.system(cmdP + ' python3 pmuNet.py PMU 1 127.0.0.1 1410 2048 true')
+os.system(cmdP + ' python3 pmuNet.py PDC 1 127.0.0.1 1410 2048 true')
+os.system(cmdP + ' python3 pmuNet.py PMU 2 127.0.0.1 1420 2048 true')
+os.system(cmdP + ' python3 pmuNet.py PDC 2 127.0.0.1 1420 2048 true')
