d050840474ee8ff40f2be6f8db4de7f8cc56850d
mishakh <kharitonovm@wit.edu>
2020-06-29 20:06:21 -0400

    getPTPTimestamps.py captures for 5 seconds and outputs all origintimestamp_seconds layer attributes

--- openPMU.py
+++ openPMU.py
@@ -16,6 +16,7 @@ pmuCall2 = cmdP + ' "python3 pmuNet.py PMU 2 localhost 1420 2048 true"'
 pdcCall2 = cmdP + ' "python3 pmuNet.py PDC 2 localhost 1420 2048 true"'
 
 proc1 = sp.Popen(pmuCall1, shell=True)
-proc2 = sp.Popen(pdcCall1, shell=True)
-proc3 = sp.Popen(pmuCall2, shell=True)
+proc2 = sp.Popen(pmuCall2, shell=True)
+proc3 = sp.Popen(pdcCall1, shell=True)
 proc4 = sp.Popen(pdcCall2, shell=True)
+
