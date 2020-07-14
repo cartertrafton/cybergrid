6931a47d0c4f9612681ac046dfbe5ed13e72c768
cartertrafton <cartertrafton@gmail.com>
2020-07-13 20:05:35 -0400

    dirty thread commit, will clean by morning

--- gui/gui.py
+++ gui/gui.py
@@ -11,16 +11,25 @@
 #
 
 import tkinter as tk
-import random, sys
+import random
+import sys
+import time
+import threading
+import queue
 from gui.pmuFrame import PmuDataDisplay
 from gui.mapFrame import SystemMapDisplay
 from gui.nodeFrame import NodeDataDisplay
 
+rand = random.Random()
+
 
 #### GUI class
 class GUI(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, queue):
         tk.Frame.__init__(self)
+
+        #### queue
+        self.queue = queue
 
         #### canvas and background
         self.canvas = tk.Canvas(parent, height=800, width=1200)
@@ -91,8 +100,83 @@ class GUI(tk.Frame):
             self.nodeDisplay.update_pmu_status(self.spoof_status, self.cybergrid_status)
             self.mapDisplay.update_map(self.spoof_status, self.cybergrid_status)
             self.change = False
-
         return
+
+    def processIncoming(self):
+        """Handle all messages currently in the queue, if any."""
+        while self.queue.qsize():
+            try:
+                msg = self.queue.get(0)
+                # Check contents of message and do whatever is needed. As a
+                # simple test, print it (in real life, you would
+                # suitably update the GUI's display in a richer fashion).
+                print(msg)
+            except queue.Empty:
+                # just on general principles, although we don't
+                # expect this branch to be taken in this case
+                pass
+
+
+#### threadedClient class
+class ThreadedClient:
+    """
+    Launch the main part of the GUI and the worker thread. periodicCall and
+    endApplication could reside in the GUI part, but putting them here
+    means that you have all the thread controls in a single place.
+    """
+    def __init__(self, parent):
+        """
+        Start the GUI and the asynchronous threads. We are in the main
+        (original) thread of the application, which will later be used by
+        the GUI as well. We spawn a new thread for the worker (I/O).
+        """
+        self.parent = parent
+
+        # Create the queue
+        self.queue = queue.Queue()
+
+        # Set up the GUI part
+        self.gui = GUI(parent, self.queue)
+
+        # Set up the thread to do asynchronous I/O
+        # More threads can also be created and used, if necessary
+        self.running = 1
+        self.thread1 = threading.Thread(target=self.workerThread1)
+        self.thread1.start()
+
+        # Start the periodic call in the GUI to check if the queue contains
+        # anything
+        self.periodicCall()
+
+    def periodicCall(self):
+        """
+        Check every 200 ms if there is something new in the queue.
+        """
+        self.gui.update_GUI()
+        self.gui.processIncoming()
+        if not self.running:
+            # This is the brutal stop of the system. You may want to do
+            # some cleanup before actually shutting it down.
+            import sys
+            sys.exit(1)
+        self.parent.after(200, self.periodicCall)
+
+    def workerThread1(self):
+        """
+        This is where we handle the asynchronous I/O. For example, it may be
+        a 'select(  )'. One important thing to remember is that the thread has
+        to yield control pretty regularly, by select or otherwise.
+        """
+        while self.running:
+            # To simulate asynchronous I/O, we create a random number at
+            # random intervals. Replace the following two lines with the real
+            # thing.
+            time.sleep(rand.random() * 1.5)
+            msg = rand.random()
+            self.queue.put(msg)
+
+    def endApplication(self):
+        self.running = 0
 
 
 #### functions
--- main.py
+++ main.py
@@ -7,8 +7,8 @@
 #
 import tkinter as tk
 import random
-from gui.gui import GUI
-from openPMUThreads import *
+from gui.gui import *
+# from openPMUThreads import *
 from threading import Thread
 from ptpSniffer import ptpSniffer, ptpPacketData
 
@@ -44,72 +44,75 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title('CyberGrid')
 
+    #### thread set up
+    client = ThreadedClient(root)
+
     #### creating GUI
-    gui = GUI(root)
-    gui.pack(side="top", fill="both", expand=True)
-    gui.update_GUI()
-
-    ptpCapture = ptpSniffer('enp3s0', capfile='/home/cybergrid/cybergrid/ptpsample.pcap')
-
-    pmu1 = PMUrun(1, '127.0.0.1', 1410, 2048, True)
-    pmu2 = PMUrun(2, '127.0.0.1', 1420, 2048, True)
-    time.sleep(0.05)
-    pdc1 = PDCrun(1, '127.0.0.1', 1410, 2048)
-    pdc2 = PDCrun(2, '127.0.0.1', 1420, 2048)
-
-    fullSeq = [False, False, False, False]
-    pdc1TSBuffer = []
-    pdc1DataBuffer = []
-    pdc2TSBuffer = []
-    pdc2DataBuffer = []
-    tsDiff = []
+    #gui = GUI(root)
+    #gui.pack(side="top", fill="both", expand=True)
+    #gui.update_GUI()
+    #
+    # ptpCapture = ptpSniffer('enp3s0', capfile='/home/cybergrid/cybergrid/ptpsample.pcap')
+    #
+    # pmu1 = PMUrun(1, '127.0.0.1', 1410, 2048, True)
+    # pmu2 = PMUrun(2, '127.0.0.1', 1420, 2048, True)
+    # time.sleep(0.05)
+    # pdc1 = PDCrun(1, '127.0.0.1', 1410, 2048)
+    # pdc2 = PDCrun(2, '127.0.0.1', 1420, 2048)
+    #
+    # fullSeq = [False, False, False, False]
+    # pdc1TSBuffer = []
+    # pdc1DataBuffer = []
+    # pdc2TSBuffer = []
+    # pdc2DataBuffer = []
+    # tsDiff = []
 
     while True:
         try:
 
-            for pack in ptpCapture.liveCapture():
-
-                if pack.mesType == 'Sync':
-                    fullSeq[1] = True
-                    syncPak = pack
-                if pack.mesType == 'Announce':
-                    fullSeq[0] = True
-                    anncPak = pack
-                if pack.mesType == 'Delay Request':
-                    fullSeq[2] = True
-                    delreqPak = pack
-                if pack.mesType == 'Delay Response':
-                    fullSeq[3] = True
-                    delresPak = pack
-                if fullSeq == [True, True, True, True]:
-                    break
-
-            anncPak.printPackInfo()
-            syncPak.printPackInfo()
-            delreqPak.printPackInfo()
-            delresPak.printPackInfo()
-            fullSeq = [False, False, False, False]
-
-            if not (len(pdc1.ts_buffer) == 0 or len(pdc2.ts_buffer) == 0):
-                pdc1TSBuffer = pdc1.ts_buffer
-                pdc1DataBuffer = pdc1.data_buffer
-                pdc2TSBuffer = pdc2.ts_buffer
-                pdc2DataBuffer = pdc2.data_buffer
-                print(len(pdc1TSBuffer), len(pdc2TSBuffer))
-                tsDiff.append((max(pdc2TSBuffer) - min(pdc2TSBuffer)) - (max(pdc1TSBuffer) - min(pdc1TSBuffer)))
-                print('\nRunning average ts difference:', sum(tsDiff) / len(tsDiff), '\n')
-                # print('delta t PMU 1:', max(pdc1TSBuffer) - min(pdc1TSBuffer),'delta t PMU 2:', max(pdc2TSBuffer) - min(pdc2TSBuffer))
-
-            # print(len(pdc2TSBuffer),len(pdc1TSBuffer))
-            pdc2.ts_buffer.clear()
-            pdc2.data_buffer.clear()
-            pdc1.ts_buffer.clear()
-            pdc1.data_buffer.clear()
+            # for pack in ptpCapture.liveCapture():
+            #
+            #     if pack.mesType == 'Sync':
+            #         fullSeq[1] = True
+            #         syncPak = pack
+            #     if pack.mesType == 'Announce':
+            #         fullSeq[0] = True
+            #         anncPak = pack
+            #     if pack.mesType == 'Delay Request':
+            #         fullSeq[2] = True
+            #         delreqPak = pack
+            #     if pack.mesType == 'Delay Response':
+            #         fullSeq[3] = True
+            #         delresPak = pack
+            #     if fullSeq == [True, True, True, True]:
+            #         break
+            #
+            # anncPak.printPackInfo()
+            # syncPak.printPackInfo()
+            # delreqPak.printPackInfo()
+            # delresPak.printPackInfo()
+            # fullSeq = [False, False, False, False]
+            #
+            # if not (len(pdc1.ts_buffer) == 0 or len(pdc2.ts_buffer) == 0):
+            #     pdc1TSBuffer = pdc1.ts_buffer
+            #     pdc1DataBuffer = pdc1.data_buffer
+            #     pdc2TSBuffer = pdc2.ts_buffer
+            #     pdc2DataBuffer = pdc2.data_buffer
+            #     print(len(pdc1TSBuffer), len(pdc2TSBuffer))
+            #     tsDiff.append((max(pdc2TSBuffer) - min(pdc2TSBuffer)) - (max(pdc1TSBuffer) - min(pdc1TSBuffer)))
+            #     print('\nRunning average ts difference:', sum(tsDiff) / len(tsDiff), '\n')
+            #     # print('delta t PMU 1:', max(pdc1TSBuffer) - min(pdc1TSBuffer),'delta t PMU 2:', max(pdc2TSBuffer) - min(pdc2TSBuffer))
+            #
+            # # print(len(pdc2TSBuffer),len(pdc1TSBuffer))
+            # pdc2.ts_buffer.clear()
+            # pdc2.data_buffer.clear()
+            # pdc1.ts_buffer.clear()
+            # pdc1.data_buffer.clear()
 
             #### GUI updating
             root.update_idletasks()
             root.update()  # update the GUI
-            gui.update_GUI()
+            #gui.update_GUI()
 
         except:
             exit()
