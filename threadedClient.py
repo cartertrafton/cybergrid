7d7557882ea722611cdf10a7b8fe30938624203a
mishakh <kharitonovm@wit.edu>
2020-07-18 14:40:44 -0400

    Push for merge

--- threadedClient.py
+++ threadedClient.py
@@ -1,8 +1,4 @@
-import time
-import threading
-import queue
 from gui.gui import *
-# from openPMUThreadsV2 import *
 import pmuThreads
 import pyshark
 from ptpSniffer import ptpSniffer, ptpPacketData
@@ -107,17 +103,15 @@ class ThreadedClient:
         # Set up the thread to do asynchronous I/O
         # More threads can also be created and used, if necessary
         self.running = 1
-        # self.thread1 = threading.Thread(target=self.workerThreads)
-        # self.thread1.start()
+
         self.thread0 = PMUrun(1, '127.0.0.1', 1410, 2048, True)
         self.thread1 = PDCrun(1, '127.0.0.1', 1410, 2048, self.qev1, self.qLock1)
         self.thread2 = PMUrun(2, '127.0.0.1', 1420, 2048, True)
         self.thread3 = PDCrun(2, '127.0.0.1', 1420, 2048, self.qev2, self.qLock2)
-        # self.thread2 = ptpThread(interface='enp3s0',dispfilter='ptp',queue= self.queue)
-        # self.thread2 = threading.Thread(target=self.ptp_worker, kwargs={'interface': 'enp3s0', 'df': 'ptp'})
+
         # Start the periodic call in the GUI to check if the queue contains
         # self.thread2.start()
-        self.ts_buffer = list()
+        self.avgDelay = list()
         self.thread0.start()
         self.thread2.start()
         sleep(0.001)
@@ -130,33 +124,29 @@ class ThreadedClient:
         """
         Check every 200 ms if there is something new in the queue.
         """
-        # print('test')
-        self.gui.update_GUI()
-        # self.thread1.ts_buffer.clear()
-
-        self.ptp_buffer.clear()
-        self.gui.processIncoming()
-
+        ts1 = False
+        ts2 = False
         try:
             #
-            # print(self.thread1.queue, self.thread1.queue.qsize(), 'recv', self.thread1.queue.full())
             self.ptpCapture()
+
             if self.qev1.isSet():
                 self.qLock1.acquire()
-
-                buff1 = self.thread1.ts_buffer.copy()
-
+                if len(self.thread1.ts_buffer) > 0:
+                    buff1 = self.thread1.ts_buffer.copy()
+                    ts1 = True
                 self.qLock1.release()
                 self.qev1.clear()
 
             if self.qev2.isSet():
                 self.qLock2.acquire()
-
-                buff2 = self.thread3.ts_buffer.copy()
-
+                if len(self.thread3.ts_buffer) > 0:
+                    buff2 = self.thread3.ts_buffer.copy()
+                    ts2 = True
                 self.qLock2.release()
                 self.qev2.clear()
 
+            if ts1 and ts2:
                 print('PMU 1\n-------------------------')
                 print(' Length:', len(buff1), ' Min:', min(buff1), ' Max:', max(buff1))
                 print(' Time Delta:', max(buff1) - min(buff1))
@@ -165,28 +155,38 @@ class ThreadedClient:
                 print(' Time Delta:', max(buff2) - min(buff2))
 
                 print('Time Differences- max:', max(buff1)-max(buff2),'min:',min(buff1)-min(buff2))
-            print('-------------')
-            for pack in self.ptp_buffer:
-                print(pack.mesType, '- time: ', pack.tsComplete)
-
-            print('-------------\nptp delayed:',((max(buff1)+max(buff2))/2)-self.ptp_buffer[0].tsComplete,'\n\n')
+                print('-------------')
+                for pack in self.ptp_buffer:
+                    print(pack.mesType, '- time: ', pack.tsComplete)
+                ptpDelay = max(buff1)-self.ptp_buffer[0].tsComplete
+                self.avgDelay.append(ptpDelay)
+                print('-------------\n')
+                print('Average Delay of PTP synchronization:', sum(self.avgDelay) / len(self.avgDelay))
+                self.ptp_buffer.clear()
+            else:
+                pass
+            self.gui.update_GUI()
             self.ptp_buffer.clear()
+            self.gui.processIncoming()
 
         except UnboundLocalError or ValueError as e:
             print(e)
+            self.parent.after(round((1000 * (1 / self.thread1.data_rate))), self.periodicCall)
 
-        if not self.running:
-            # This is the brutal stop of the system. You may want to do
-            # some cleanup before actually shutting it down.
-            import sys
-            sys.exit(1)
-        self.parent.after(round((1000 * (1 / self.thread1.data_rate))), self.periodicCall)
-        # self.parent.after(5, self.periodicCall)
+        finally:
+            if not self.running:
+                # This is the brutal stop of the system. You may want to do
+                # some cleanup before actually shutting it down.
+
+                import sys
+                sys.exit(1)
+            self.parent.after(round((1000 * (1 / self.thread1.data_rate))), self.periodicCall)
+            # self.parent.after(5, self.periodicCall)
+
 
 
     def ptpCapture(self):
-
-        for pak in cap.sniff_continuously(packet_count=1):
+        for pak in cap.sniff_continuously(packet_count=5):
             self.ptp_buffer.append(p.assignPack(pak))
 
         cap.clear()
